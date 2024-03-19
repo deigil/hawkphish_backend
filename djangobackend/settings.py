451d3797f1554419c95b9b9affc312a8520b8aab
Sam Karson <samkarson94@gmail.com>
2024-03-18 20:12:21 -0500

    changed REST parser

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -130,3 +130,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': (
+            'rest_framework.parsers.JSONParser',
+            'rest_framework.parsers.FormParser',
+            'rest_framework.parsers.MultiPartParser',
+    ),
+}
