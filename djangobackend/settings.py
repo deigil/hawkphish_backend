17e1e65b002fc97b5fd67113905302c149942ee5
Sam Karson <samkarson94@gmail.com>
2024-04-04 11:05:28 -0500

    allowing all origin CORS

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -120,6 +120,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True  # Allow requests from any origin
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
