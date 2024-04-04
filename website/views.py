c82756a4f38d96ddc4c43f594bbb522690e1420a
Sam Karson <samkarson94@gmail.com>
2024-04-04 09:48:27 -0500

    html fix?v4

--- website/views.py
+++ website/views.py
@@ -3,4 +3,4 @@ from database.models import *
 
 def display_links(request):
     links = updatedLink.objects.all()  # Retrieve all instances of the Links model
-    return render(request, 'list_links.html', {'links': links})
+    return render(request, 'list_links.html', {'updatedLink': links})
